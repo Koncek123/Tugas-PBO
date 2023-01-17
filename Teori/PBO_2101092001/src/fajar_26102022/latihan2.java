589f1a3351f6c92be2391107921008a678c0ac72
Adi Muhamad Fajar <adimuhamadfajar@gmail.com>
2023-01-17 10:05:04 +0700

    Modul 7

---
+++ Teori/PBO_2101092001/src/fajar_26102022/latihan2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fajar_26102022;
+
+/**
+ *
+ * @author ACER
+ */
+public class latihan2 {
+    public static void main(String[] args) {
+        String days[]= {"Minggu","Senin","Selasa","Rabu","Kamis",
+                         "Jum'at","Sabtu"};
+        
+        for (int i=0;i<days.length;i++){
+            System.out.print(days[i]+"\t");
+        }
+                
+    }
+}
