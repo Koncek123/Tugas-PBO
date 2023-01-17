a3668c24472b5b6538414734042b309ac2101320
Adi Muhamad Fajar <adimuhamadfajar@gmail.com>
2023-01-17 11:43:55 +0700

    Modul 11 lat 2

---
+++ Teori/PBO_2101092001/src/Modul11_lat2/Shape.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul11_lat2;
+
+/**
+ *
+ * @author ACER
+ */
+public abstract class Shape {
+    public abstract double getArea();
+    public abstract String getName();
+}
